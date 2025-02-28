cc3fcc845590cb529a326816531e5e755fd955bf
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-02-28 15:42:26 -0800

    added header

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -15,11 +15,13 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
       {/* Header */}
+      <header className="flex justify-between items-center p-4 border-b border-gray-800">
 
+      </header>
 
       {/* Main Stuff */}
       <h1 className="text-4xl font-bold">Hello World :D</h1>
-      
+
 
       {/* Footer */}
     </div>
@@ -30,10 +32,21 @@ export default function Home() {
 
 /*
 <div className="flex flex-col min-h-screen bg-gray-900 text-white">
-  flex:           Flexbox layout system
-  flex-col:       order container children vertically/in columns (default is horizontally/in rows)
-  min-h-screen:   minimum height of the container is the height of the screen >>> full-screen
-  bg-gray-900:    background color (100-900???)
-  text-white:     white text :D
+  flex:               flexbox layout system
+  flex-col:           order container children vertically/in columns (default is horizontally/in rows)
+  min-h-screen:       minimum height of the container is the height of the screen >>> full-screen
+  bg-gray-900:        background color (100-900???)
+  text-white:         white text :D
+*/
 
+/*
+<header className="flex justify-between items-center p-4 border-b border-gray-800">
+  flex:               flexbox layout system
+  justify-between:    distributes items with maximum space between them (~ justify text-aligment)
+  items-center:       vertically center items within header container
+  p-4:                padding (left, right, top, bottom) of value 4 in Tailwind's spacing scale
+  border-b:           adds a bottom border
+  border-gray-800:    sets border color to gray
+
+Note: I am not sure why this component spawned a "dev tool" button on the bottom left corner of the website
 */
