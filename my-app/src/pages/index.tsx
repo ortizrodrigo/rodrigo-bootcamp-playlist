57a895b3b7efc834a19a70b2b4558a6ce9913d40
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-02-28 15:27:35 -0800

    added background color

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -13,6 +13,27 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <h1 className="text-4xl font-bold">Hello World</h1>
+    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+      {/* Header */}
+
+
+      {/* Main Stuff */}
+      <h1 className="text-4xl font-bold">Hello World :D</h1>
+      
+
+      {/* Footer */}
+    </div>
+    //<h1 className="text-4xl font-bold">Hello World</h1>
   );
 }
+
+
+/*
+<div className="flex flex-col min-h-screen bg-gray-900 text-white">
+  flex:           Flexbox layout system
+  flex-col:       order container children vertically/in columns (default is horizontally/in rows)
+  min-h-screen:   minimum height of the container is the height of the screen >>> full-screen
+  bg-gray-900:    background color (100-900???)
+  text-white:     white text :D
+
+*/
